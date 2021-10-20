c8df62aaeefbdbe97c230dfcbe1f92993faaddb5
Alexander Kirillov <shurik179@gmail.com>
2021-10-20 13:03:21 -0400

    fixing typo in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ html_theme_options = {
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-h#tmlhelp_basename = 'RoverWingLibrarySitedoc'
+#htmlhelp_basename = 'RoverWingLibrarySitedoc'
 
 
 
